9c22ae646b11fd87b121b140974c33623121cf26
Mohamed-Omar <mero2online@gmail.com>
2020-11-30 01:41:27 +0200

    Make all function and req

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -5,24 +5,41 @@ function handleSubmit(event) {
   let lat = document.getElementById('lat').value;
   let lon = document.getElementById('lon').value;
   // Client.checkForName(zipCode);
-  // fetch api key from server to make API call
-  fetch('/api')
-    .then((data) => data.json())
-    .then(function (data) {
-      const apiKey = data.apiKey;
-      // Base URL for OpenWeatherMap API to optain current weather data by ZIP code
-      let baseURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=`;
 
-      console.log('::: Form Submitted :::');
-      fetch(baseURL + apiKey)
-        .then((res) => res.json())
-        .then(function (res) {
-          document.getElementById(
-            'resultsTemp'
-          ).innerHTML = `Temp: ${res.main.temp}<sup>o</sup> C`;
-          document.getElementById('resultsName').innerHTML = res.name;
-          document.getElementById('resultsCountry').innerHTML = res.sys.country;
-        });
-    });
+  console.log('::: Form Submitted :::');
+  postData('/coordinateData', {
+    lat: lat,
+    lon: lon,
+  }).then(function (res) {
+    console.log(res);
+    var allData = JSON.stringify(res);
+    console.log(allData);
+    document.getElementById(
+      'resultsTemp'
+    ).innerHTML = `Temp: ${res.main.temp}<sup>o</sup> C`;
+    document.getElementById('resultsName').innerHTML = res.name;
+    document.getElementById('resultsCountry').innerHTML = res.sys.country;
+  });
 }
+/* Function to POST data */
+const postData = async (url = '', data = {}) => {
+  console.log('postData', data);
+  const res = await fetch(url, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+
+  try {
+    const newData = await res.json();
+    return newData;
+  } catch (error) {
+    console.log('error', error);
+    // appropriately handle the error
+  }
+};
+
 export { handleSubmit };
--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,7 @@
 var path = require('path');
 const express = require('express');
 const mockAPIResponse = require('./mockAPI.js');
-
+const fetch = require('node-fetch');
 const app = express();
 
 const dotenv = require('dotenv');
@@ -13,10 +13,12 @@ const apiKey = process.env.API_KEY;
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+const bodyParser = require('body-parser');
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
@@ -40,12 +42,22 @@ app.get('/test', function (req, res) {
   res.send(mockAPIResponse);
 });
 
-const apiKeyData = {
-  apiKey: apiKey,
-};
+// POST Route
+app.post('/coordinateData', coordinateData);
 
-app.get('/api', getApiKeyData);
+async function coordinateData(req, res) {
+  console.log('post req.body', req.body);
+  let cooData = req.body;
+  lat = cooData.lat;
+  lon = cooData.lon;
 
-function getApiKeyData(req, res) {
-  res.send(apiKeyData);
+  console.log('lat val', lat);
+  console.log('lon val', lon);
+  // Base URL for OpenWeatherMap API to optain current weather data by ZIP code
+  let baseURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=`;
+  let apiURL = baseURL + apiKey;
+  const response = await fetch(apiURL)
+  const mcData = await response.json()
+  console.log(mcData)
+  res.send(mcData)
 }
