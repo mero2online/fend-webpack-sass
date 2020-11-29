a9b9b8b57abe9454cfbca2249fa48df43b7dee2a
Mohamed-Omar <mero2online@gmail.com>
2020-11-29 22:06:56 +0200

    Send and receive api key

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,6 +1,3 @@
-// Personal API Key for OpenWeatherMap API
-const apiKey = process.env.API_KEY;
-
 function handleSubmit(event) {
   event.preventDefault();
 
@@ -8,18 +5,24 @@ function handleSubmit(event) {
   let lat = document.getElementById('lat').value;
   let lon = document.getElementById('lon').value;
   // Client.checkForName(zipCode);
+  // fetch api key from server to make API call
+  fetch('/api')
+    .then((data) => data.json())
+    .then(function (data) {
+      const apiKey = data.apiKey;
+      // Base URL for OpenWeatherMap API to optain current weather data by ZIP code
+      let baseURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=`;
 
-  // Base URL for OpenWeatherMap API to optain current weather data by ZIP code
-  let baseURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=`;
-
-  console.log('::: Form Submitted :::');
-  fetch(baseURL + apiKey)
-    .then((res) => res.json())
-    .then(function (res) {
-      document.getElementById('resultsTemp').innerHTML = `Temp: ${res.main.temp}<sup>o</sup> C`;
-      document.getElementById('resultsName').innerHTML = res.name;
-      document.getElementById('resultsCountry').innerHTML = res.sys.country;
+      console.log('::: Form Submitted :::');
+      fetch(baseURL + apiKey)
+        .then((res) => res.json())
+        .then(function (res) {
+          document.getElementById(
+            'resultsTemp'
+          ).innerHTML = `Temp: ${res.main.temp}<sup>o</sup> C`;
+          document.getElementById('resultsName').innerHTML = res.name;
+          document.getElementById('resultsCountry').innerHTML = res.sys.country;
+        });
     });
 }
-
 export { handleSubmit };
--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,8 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+var path = require('path');
+const express = require('express');
+const mockAPIResponse = require('./mockAPI.js');
 
-const app = express()
+const app = express();
 
 const dotenv = require('dotenv');
 dotenv.config();
@@ -11,19 +11,29 @@ console.log(`Your API key is ${process.env.API_KEY}`);
 // Personal API Key for MeaningCloud API
 const apiKey = process.env.API_KEY;
 
-app.use(express.static('dist'))
+app.use(express.static('dist'));
 
-console.log(__dirname)
+console.log(__dirname);
 
 app.get('/', function (req, res) {
-    res.sendFile('dist/index.html')
-})
+  res.sendFile('dist/index.html');
+});
 
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
-    console.log('Example app listening on port 8081!', `http://localhost:8081/`)
-})
+  console.log('Example app listening on port 8081!', `http://localhost:8081/`);
+});
 
 app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+  res.send(mockAPIResponse);
+});
+
+const apiKeyData = {
+  apiKey: apiKey,
+};
+
+app.get('/api', getApiKeyData);
+
+function getApiKeyData(req, res) {
+  res.send(apiKeyData);
+}
